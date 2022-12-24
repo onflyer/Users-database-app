7e3dc52433b9236454cdde51f019f8d0940c4818
onflyer <salkaorama@gmail.com>
2022-12-24 14:26:54 +0100

    Initial Commit

---
+++ SampleApp/SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppApp.swift
+//  SampleApp
+//
+//  Created by Aleksandar Milidrag on 12/24/22.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
